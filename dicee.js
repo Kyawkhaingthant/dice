5518a721f5e7d9bb5dff8b28645f147f76f3f100
Kyaw Khaing Thant <tonyharrison421@gmail.com>
2024-03-23 14:38:17 +0630

    java dice

---
+++ dicee.js
@@ -0,0 +1,7 @@
+var ramdomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var ramdomDiceImage = "dice" + ramdomNumber1 + ".png";
+
+var ramdomImageSource = "images/" + ramdomDiceImage;
+
+document.querySelectorAll
